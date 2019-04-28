25b0ab11eec648d937ab55b491f7845b0a225470
Ricardo MB <ladraum@gmail.com>
2019-04-27 18:04:24 -0700

    Added Axios to load data from server

--- src/Album/AlbumGrid.test.js
+++ src/Album/AlbumGrid.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AlbumGrid from './AlbumGrid';
-import TestRenderer from "react-test-renderer";
 
 describe('AlbumGrid', () => {
   it('renders without crashing', () => {
